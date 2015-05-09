f1606ba75a08a09a2520b9215e092b5ee5f0c447
Kepler Sticka-Jones <kepler+git@stickajones.org>
2015-05-09 00:22:41 -0600

    Redid iOS specialization.

--- src/llvm/abi/arm.cr
+++ src/llvm/abi/arm.cr
@@ -2,83 +2,50 @@ require "../abi"
 
 # Based on https://github.com/rust-lang/rust/blob/master/src/librustc_trans/trans/cabi_arm.rs
 class LLVM::ABI::ARM < LLVM::ABI
-  #Woot, Apple diversification.
-  enum Flavor
-    General
-    IOS
-  end
-
   def abi_info(atys: Array(Type), rty: Type, ret_def: Bool)
-    abi_info(atys, rty, ret_def, Flavor::General)
+    ret_ty = compute_return_type(rty, ret_def)
+    arg_tys = compute_arg_types(atys)
   end
 
-  def abi_info(atys: Array(Type), rty: Type, ret_def: Bool, flav: Flavor)
-    align_fn = case flav
-    when Flavor::General then general_align(rty)
-    when Flavor::IOS then ios_align(rty)
-    end
-  end
-
-  ##
-  # iOS related functions
-  ##
-
-  # For more information see:
+  # For more information on iOS see:
   # ARMv7
   # https://developer.apple.com/library/ios/documentation/Xcode/Conceptual
   #    /iPhoneOSABIReference/Articles/ARMv7FunctionCallingConventions.html
   # ARMv6
   # https://developer.apple.com/library/ios/documentation/Xcode/Conceptual
   #    /iPhoneOSABIReference/Articles/ARMv6FunctionCallingConventions.html
-  private def ios_align(type: Type)
+  private def align(type: Type)
     case type.kind
-    when Type::Kind::Integer then Math.min(4, (((type.int_width) + 7) / 8))
-    when Type::Kind::Pointer then 4
-    when Type::Kind::Float then 4
-    when Type::Kind::Double then 4
+    when Type::Kind::Integer
+      if is_ios
+        Math.min(4, ((type.int_width + 7) / 8))
+      else
+        (type.int_width + 7) / 8
+      end
+    when Type::Kind::Pointer
+      4
+    when Type::Kind::Float
+      4
+    when Type::Kind::Double
+      if is_ios
+        4
+      else
+        8
+      end
     when Type::Kind::Struct
       if type.packed_struct?
         1
       else
         type.struct_element_types.inject(1) do |a, t|
-          Math.max(a, ios_align(t))
+          Math.max(a, align(t))
         end
       end
     when Type::Kind::Array then ios_align(type.element_type)
-    #when Type::Kind::Vector then ios_align(type.element_type) * type.vector_length
+    #when Type::Kind::Vector then align(type.element_type) * type.vector_length
     else
       raise "Unhandled Type::Kind: #{type.kind}"
     end
   end
-
-  ##
-  # General ARM related functions
-  ##
-
-  private def general_align(type: Type)
-    case type.kind
-    when Type::Kind::Integer then ((type.int_width) + 7) / 8
-    when Type::Kind::Pointer then 4
-    when Type::Kind::Float then 4
-    when Type::Kind::Double then 8
-    when Type::Kind::Struct
-      if type.packed_struct?
-        1
-      else
-        type.struct_element_types.inject(1) do |a, t|
-          Math.max(a, general_align(t))
-        end
-      end
-    when Type::Kind::Array then general_align(type.element_type)
-    #when Type::Kind::Vector then general_align(type.element_type) * type.vector_length
-    else
-      raise "Unhandled Type::Kind: #{type.kind}"
-    end
-  end
-
-  ##
-  # ARM functions
-  ##
 
   private def reg_type?(type: Type)
     case type.kind
@@ -128,5 +95,11 @@ class LLVM::ABI::ARM < LLVM::ABI
     else
       raise "Unhandled Type::Kind: #{type.kind}"
     end
+  end
+
+  private def compute_return_type(rty, ret_def)
+  end
+
+  private def compute_arg_types(atys)
   end
 end
